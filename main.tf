9fd39063caa0ef210bb9f74594daff9f3fcb8588
root <root@MSI-D00078>
2024-11-22 10:39:34 +0800

    creates S3 buckets

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "bucket_1" {
+    bucket = "ce8-tschui-1"
+}
+
+resource "aws_s3_bucket" "bucket_2" {
+    bucket = "ce8-tschui-2"
+}
+
+resource "aws_s3_bucket" "bucket_3" {
+    bucket = "ce8-tschui-3"
+}
